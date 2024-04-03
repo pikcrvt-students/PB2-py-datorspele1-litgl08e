da059926d96ba3a7d2c654952e54fcb11248f72e
Gļebs Litviņenko <a230111gl@rvt.lv>
2024-04-03 15:24:16 +0300

    papildinu datorspeles kodu ar iespejas skaititaju un kļūdu apstrādātāju, izlaboju kļūdas. Pievienoju readme failam noteikumus par kļūdu apstrādātāju un iespējam.

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 Dators ģenerē skaitli diapazonā no 1 līdz 100.
 Lietotājam jāuzmina skaitļi ar 10 iespējam. 
 
-Lietotājs ievada jeb kuru skaitli no 1 līdz 100. Ja viņš neuzminēja, tad programma izvada vai "iedomāts" skaitlis ir lielāks vai mazāks par ievadīto.
+Lietotājs ievada jeb kuru skaitli no 1 līdz 100(ieskaitot). Ja viņš neuzminēja, tad programma izvada vai "iedomāts" skaitlis ir lielāks vai mazāks par ievadīto. Pirms ievadam programma izvada, kāda iespeja Jums ir tagad. Ja lietotājs ievada skaitli, kas ir mazāks par 1 vai lielāks par 100, tad programma izvada kļūdu un iespēja neskaitās.
 
 Ja lietotājs neuzminēja skaitli ar 10 iespējam, tad spēle beigas un dators izvada ASCII tekstu ar animāciju "Neuzmineja :(", bet ja uzminēja, tad viņš ir malacis, un izvadas teksts ar animāciju "Uzmineja!!!".
--- datorspele.py
+++ datorspele.py
@@ -1,47 +1,52 @@
 from random import randint
-import time
-import os
+import time, os
 print('Datorspēle \'Uzmini skaitli\'.')
-print('Dators "iedomājas" veselo skaitli no 1 līdz 10. ', end='\n\n')
+print('Lietotājam ir 10 iespējas, lai uzminēt skaitli.')
+print('Dators "iedomājas" veselo skaitli no 1 līdz 100(ieskaitot). ', end='\n\n')
 print('Uzminiet skaitli. ')
 datora_skaitlis = randint(1,100)
-iespeja = 9
+iespeja = 1
 
 neuzmineja = True
 while neuzmineja:
-    ievade = input("Ievadiet skaitli no 1 lidz 100, ieskaitot 100: ")
+    print(iespeja, "iespeja no 10.")
+    ievade = input("Ievadiet skaitli no 1 lidz 100, ieskaitot 1 un 100: ")
     lietotaja_skaitlis = int(ievade)
-    if iespeja == 0:
+    if lietotaja_skaitlis < 1:
+        print("Kļūda! Ievadiet pareizo skaitli (no 1 līdz 100).")
+    elif lietotaja_skaitlis > 100:
+        print("Kļūda! Ievadiet pareizo skaitli (no 1 līdz 100).")
+    elif iespeja == 10:
         neuzmineja = False
-        print(" _______                              .__                   __        __            ___", end='\n', flush=True)
+        print(" _______                              .__                   __        __            ___")
         time.sleep(1)
-        print(" \      \   ____  __ __________ _____ |__| ____   ____     |__|____ _/  |_   /\    /  /", end='\n', flush=True)
+        print(" \      \   ____  __ __________ _____ |__| ____   ____     |__|____ _/  |_   /\    /  /")
         time.sleep(1)
-        print(" /   |   \_/ __ \|  |  \___   //     \|  |/    \_/ __ \    |  \__  \\   __\   \/   /  / ", end='\n', flush=True)
+        print(" /   |   \_/ __ \|  |  \___   //     \|  |/    \_/ __ \    |  \__  \\   __\   \/   /  / ")
         time.sleep(1)
-        print("/    |    \  ___/|  |  //    /|  Y Y  \  |   |  \  ___/    |  |/ __ \|  |    /\  (  (  ", end='\n', flush=True)
+        print("/    |    \  ___/|  |  //    /|  Y Y  \  |   |  \  ___/    |  |/ __ \|  |    /\  (  (  ")
         time.sleep(1)
-        print("\____|__  /\___  >____//_____ \__|_|  /__|___|  /\___  >\__|  (____  /__|    \/   \  \ ", end='\n', flush=True)
+        print("\____|__  /\___  >____//_____ \__|_|  /__|___|  /\___  >\__|  (____  /__|    \/   \  \ ")
         time.sleep(1)
-        print("        \/     \/            \/     \/        \/     \/\______|    \/              \__\\", end='\n', flush=True)
+        print("        \/     \/            \/     \/        \/     \/\______|    \/              \__\\")
     elif datora_skaitlis < lietotaja_skaitlis:
         print ( "Neuzminējāt. Ievadiet mazāku skaitli. ")
-        iespeja = iespeja - 1
+        iespeja = iespeja + 1
     else:
         if datora_skaitlis > lietotaja_skaitlis:
             print ("Neuzminējāt. Ievadiet lielāku skaitli. ")
-            iespeja = iespeja - 1
+            iespeja = iespeja + 1
         else:
             neuzmineja = False
-            print(" ____ ___                 __                   __        __  ._._._.", end='\n', flush=True)
+            print(" ____ ___                 __                   __        __  ._._._.")
             time.sleep(1)
-            print("|    |   \________ _____ |__| ____   ____     |__|____ _/  |_| | | |", end='\n', flush=True)
+            print("|    |   \________ _____ |__| ____   ____     |__|____ _/  |_| | | |")
             time.sleep(1)
-            print("|    |   /\___   //     \|  |/    \_/ __ \    |  \__  \\   __\ | | |", end='\n', flush=True)
+            print("|    |   /\___   //     \|  |/    \_/ __ \    |  \__  \\   __\ | | |")
             time.sleep(1)
-            print("|    |  /  /    /|  Y Y  \  |   |  \  ___/    |  |/ __ \|  |  \|\|\|", end='\n', flush=True)
+            print("|    |  /  /    /|  Y Y  \  |   |  \  ___/    |  |/ __ \|  |  \|\|\|")
             time.sleep(1)
-            print("|______/  /_____ \__|_|  /__|___|  /\___  >\__|  (____  /__|  ______", end='\n', flush=True)
+            print("|______/  /_____ \__|_|  /__|___|  /\___  >\__|  (____  /__|  ______")
             time.sleep(1)
-            print("                \/     \/        \/     \/\______|    \/      \/\/\/", end='\n', flush=True)
+            print("                \/     \/        \/     \/\______|    \/      \/\/\/")
 os.system("PAUSE")
